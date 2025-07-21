64ce768f4fbea64eb32a3d9c9f71dff33c091819
darpanhh <162995199+darpanhh@users.noreply.github.com>
2025-07-21 23:36:40 +0545

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+export default function TodoList(){
+  return(
+    <>
+      <h1>Hedy Lamar's Todos</h1>
+      <img
+      src="https://i.imgur.com/yXOvdOSs.jpg"
+      alt="Hedy Lamar"
+      className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Reherse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul> 
+    </>
+  )
+}
